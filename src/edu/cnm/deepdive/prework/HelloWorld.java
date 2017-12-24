cdf22e288428f5dfdd9a233cede0bf12779572ed
jacchevr <jacchevr@gmail.com>
2017-12-23 23:04:24 -0700

    Jump to end of isntructions to have completed project.

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -3,7 +3,14 @@ package edu.cnm.deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    salute(salutation(args));
   }
 
+  private static String salutation(String[] args) {
+    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+  }
+
+  private static void salute(String salutation) {
+    System.out.println(salutation);
+  }
 }
